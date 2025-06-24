ec7446d1688393b42000fb93bfe78afb4b7a44dd
Padmavathi-SJ <padmasj54@gmail.com>
2025-06-24 12:08:34 +0530

    Functions completed

--- Functions & Events/Functions.js
+++ Functions & Events/Functions.js
@@ -73,10 +73,11 @@ const func2 = (d) => d + 10;
 console.log(num2(10, func2));
 
 //Anonymous Functions - a function without a name, used as args to other functions
+/*
 setTimeout(function () {
     console.log("Anonymous functions executed!");
-}, 2000);
-
+}, 500);
+*/
 
 //Nested Functions
 function outerFun(a) {
@@ -93,5 +94,84 @@ function pureAdd(a, b) {
 }
 console.log(pureAdd(2, 3));
 
+//Arrow Functions
 const add = (a, b) => a + b;
 console.log(add(5, 3));
+
+const sub = (x, y) => x-y;
+console.log(sub(10, 5));
+
+const gfg = () => {
+    console.log("Hi from Presidio");
+}
+gfg();
+
+const square = x => x * x;
+console.log(square(4));
+
+const mult = (x, y, z) => {
+    console.log(x + y + z)
+}
+mult(10, 20, 30);
+
+const ass = (x, y, z=30) => {
+    console.log(x + " " + y + " " + z);
+}
+ass(10, 20);
+
+const makePerson = (firstName, lastName) => 
+    ({first: lastName, last: firstName});
+console.log(makePerson("Padma", "vathi"));
+
+//get Function
+const obj = {
+    arr : ["padma", "Presidio"],
+    get fun1() {
+        if (this.arr.length == 0) return undefined;
+        return this.arr[this.arr.length - 1];
+    }
+};
+console.log(obj.fun1);
+
+class func {
+    constructor() {
+        this._count = 1;
+    }
+    get count() {
+        return this._count;
+    }
+}
+const obj1 = new func();
+console.log(obj1.count);
+
+const obj2 = {
+    get func2() {
+        return "This is a getter Function";
+    }
+};
+console.log(obj2.func2);
+delete obj2.func2;
+console.log(obj2.func2);
+
+const obj3 = {};
+Object.defineProperty(obj3, "hi", {
+    get: function () {
+        return "Padma";
+    }
+});
+console.log(obj3.hi);
+
+const prop = "hi";
+const obj4 = {
+    get [prop]() {
+        return "This is a computed property name ";
+    }
+}
+console.log(obj4.hi);
+
+class GFG {
+    static get Property() {
+        return "This is a static getter";
+    }
+}
+console.log(GFG.Property);
